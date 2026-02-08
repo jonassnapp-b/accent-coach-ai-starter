d6fbdeeeffe532783b99d4d834c6428dcab14f77
Jonas <jonas@MacBook-Pro-tilhrende-Jonas.local>
2026-02-08 11:30:02 +0100

    Update

--- api/analyze-speech.js
+++ api/analyze-speech.js
@@ -169,7 +169,7 @@ function makeConnectStart({ appKey, secretKey, userId, coreType, refText, dictDi
         model: "non_native",
         scale: 100,
         precision: 0.6,
-        slack: -1,
+        slack: -0.6,
         tokenId,
       },
     },
