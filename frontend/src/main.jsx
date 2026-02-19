7148e47eeec97fb3a3ff6c3ab63530e5d779bd10
Jonas <jonas@MacBook-Pro-tilhrende-Jonas.local>
2026-02-19 18:11:53 +0100

    Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,12 +19,11 @@ function ErrorBoundary({ children }) {
     return <pre style={{ padding: 16 }}>{String(e)}</pre>;
   }
 }
-console.log("POSTHOG KEY:", import.meta.env.VITE_PUBLIC_POSTHOG_KEY);
-console.log("POSTHOG HOST:", import.meta.env.VITE_PUBLIC_POSTHOG_HOST);
+
 
 const posthogOptions = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  capture_pageview: false,
+  capture_pageview: true,
   capture_pageleave: true,
   session_recording: { enabled: false }, // vi holder replay fra for nu
 };
