b14f6c7e50f0db00b75a50c1d699092e5d7ca1ef
Jonas <jonas@MacBook-Pro-tilhrende-Jonas.local>
2026-03-14 17:34:10 +0100

    Update

--- frontend/src/lib/realtimeConversation.js
+++ frontend/src/lib/realtimeConversation.js
@@ -290,7 +290,7 @@ console.log("[realtimeConversation] realtime-session URL =", `${base}/api/realti
 
         remoteSourceNode = remoteAudioContext.createMediaStreamSource(stream);
         remoteGainNode = remoteAudioContext.createGain();
-        remoteGainNode.gain.value = 0.15;
+        remoteGainNode.gain.value = 0.5;
 
         remoteSourceNode.connect(remoteGainNode);
         remoteGainNode.connect(remoteAudioContext.destination);
